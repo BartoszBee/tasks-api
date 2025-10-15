15149d787de33a30fc819f4671c1e4b88d288e07
bartoszb <b.biernacki@delta.poznan.pl>
2025-10-15 11:54:53 +0200

    problems handled

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -4,6 +4,6 @@ import { TasksService } from './tasks.service';
 
 @Module({
   controllers: [TasksController], // drzwi do naszego pokoju
-  providers: [TasksService],      // serwis, czyli logika i dane
+  providers: [TasksService], // serwis, czyli logika i dane
 })
 export class TasksModule {}
